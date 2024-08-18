fc61c31c6cfdec3f06838c69cbfe2bddf95211c9
root <root@lyx>
2024-08-18 21:24:18 +0800

    “update”

--- train_classification.py
+++ train_classification.py
@@ -32,7 +32,7 @@ def parse_args():
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
 
     #parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
-    parser.add_argument('--batch_size', type=int, default=121, help='batch size in training') #为了把显存吃满（原来是3G/16G），把batch_size提高5倍
+    parser.add_argument('--batch_size', type=int, default=120, help='batch size in training') #为了把显存吃满（原来是3G/16G），把batch_size提高5倍
 
     parser.add_argument('--model', default='pointnet_cls', help='model name [default: pointnet_cls]')
     parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
